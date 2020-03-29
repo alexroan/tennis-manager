7dca23bc8c5bda33f4632cfbfcdfa4de12f89f9a
Alex Roan <alex.roan@hotmail.com>
2020-03-29 21:21:21 +0100

    Renamed events in TrainableTennisPlayer to present tense instead of past tense

--- contracts/TrainableTennisPlayer.sol
+++ contracts/TrainableTennisPlayer.sol
@@ -18,8 +18,8 @@ contract TrainableTennisPlayer is TennisPlayerBase {
 
     enum Attribute { agility, power, stamina, technique }
 
-    event Trained(uint indexed playerId, Attribute attribute);
-    event Rested(uint indexed playerId);
+    event Train(uint indexed playerId, Attribute attribute);
+    event Rest(uint indexed playerId);
 
     // Train a player increasing an attribute
     function train(uint _id, Attribute _attr) public {
@@ -44,7 +44,7 @@ contract TrainableTennisPlayer is TennisPlayerBase {
         else if (_attr == Attribute.technique) {
             players[_id].technique = castAdd8(players[_id].technique, attributeGainOnTrain);
         }
-        emit Trained(_id, _attr);
+        emit Train(_id, _attr);
     }
 
     // Rest player, increasing condition
@@ -55,6 +55,6 @@ contract TrainableTennisPlayer is TennisPlayerBase {
         // Must have enough XP
         players[_id].xp = castSubtract256(players[_id].xp, xpCostToRest);
         players[_id].condition = castAdd8(players[_id].condition, conditionGainOnRest);
-        emit Rested(_id);
+        emit Rest(_id);
     }
 }
